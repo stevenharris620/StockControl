abf53a90c4c42c223c0d215a25f2ba319154a2ed
Steven Harris <Steven.Harris@chi.swan.ac.uk>
2022-01-05 16:49:09 +0000

    Exceptions

---
+++ StockControl.API/Exceptions/AlreadyExistsException.cs
@@ -0,0 +1,9 @@
+﻿namespace StockControl.API.Exceptions
+{
+    public class AlreadyExistsException : Exception
+    {
+        public AlreadyExistsException(string message) : base(message)
+        {
+        }
+    }
+}
---
+++ StockControl.API/Exceptions/ValidationException.cs
@@ -0,0 +1,12 @@
+﻿namespace StockControl.API.Exceptions
+{
+    public class ValidationException : Exception
+    {
+        public ValidationException(string message, string[] errors) : base(message)
+        {
+            Errors = errors;
+        }
+
+        public string[] Errors { get; set; }
+    }
+}
