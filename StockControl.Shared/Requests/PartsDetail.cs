cb6d5c2ae15bf60fd5894b6cf33621b17eeb802a
Steven Harris <Steven.Harris@chi.swan.ac.uk>
2022-01-07 16:54:32 +0000

    PartsDetail

---
+++ StockControl.Shared/Requests/PartsDetail.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+
+namespace StockControl.Shared.Requests
+{
+    public class PartsDetail
+    {
+        public string Id { get; set; }
+        [Required] public string? Name { get; set; }
+        public string? PartCode { get; set; }
+        public string? Description { get; set; }
+        public double Cost { get; set; }
+        public string? UnitType { get; set; }
+        public int StockLevel { get; set; }
+        public int ReorderLevel { get; set; }
+
+        public IFormFile? Image { get; set; } // submit file from client to server
+
+        public string? SupplierId { get; set; }
+
+        // Cosmetic
+        public string? ImageChar64 { get; set; }
+    }
+}
