0a98e36bf5cbc8310c908a28dd47291f2b6114f3
Steven Harris <Steven.Harris@chi.swan.ac.uk>
2022-01-09 21:14:36 +0000

    partform

--- StockControl/Services/IPartService.cs
+++ StockControl/Services/IPartService.cs
@@ -1,6 +1,8 @@
-﻿using StockControl.Shared.Helpers;
+﻿using StockControl.Services.Exceptions;
+using StockControl.Shared.Helpers;
 using StockControl.Shared.Requests;
 using StockControl.Shared.Response;
+using System.Net.Http.Json;
 
 namespace StockControl.Services
 {
@@ -14,9 +16,39 @@ namespace StockControl.Services
     }
     public class PartService : IPartService
     {
-        public Task<ApiResponse<PagedList<PartDetail>>> GetPlayersAsync(string query = null, int pageNumber = 1, int pageSize = 10)
+        private readonly HttpClient _httpClient;
+
+        public PartService(HttpClient httpClient)
         {
-            throw new NotImplementedException();
+            _httpClient = httpClient;
+        }
+
+        public async Task<ApiResponse<PagedList<PartDetail>>> GetPlayersAsync(string query, int pageNumber = 1, int pageSize = 10)
+        {
+            try
+            {
+                if (pageNumber < 1) pageNumber = 1;
+                query = "a";
+                var response = await _httpClient.GetAsync($"api/part/parts?query={query}&pageNumber={pageNumber}&pageSize={pageSize}");
+
+
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var result = await response.Content.ReadFromJsonAsync<ApiResponse<PagedList<PartDetail>>>();
+                    return result;
+                }
+                else
+                {
+                    var errorResponse = await response.Content.ReadFromJsonAsync<ApiErrorResponse>();
+                    throw new ApiException(errorResponse, response.StatusCode);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
         }
 
         public Task<ApiResponse<PartDetail>> GetByIdAsync(string id)
